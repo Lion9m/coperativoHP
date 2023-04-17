3aea730660b50e3752a5daa0e0d0dc036d4c9ca4
Lion9m <marmiguelferreira9@gmail.com>
2023-04-17 14:36:58 -0300

    1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Bibliotecas
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
 import { PersonagensComponent } from './personagens/personagens.component';
 import { ContatoComponent } from './contato/contato.component';
 import { InfoComponent } from './info/info.component';
@@ -13,7 +24,6 @@ import { Home1Component } from './home1/home1.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     PersonagensComponent,
     ContatoComponent,
     InfoComponent,
@@ -22,7 +32,17 @@ import { Home1Component } from './home1/home1.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatTabsModule,
+    MatTabsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/personagens/personagens.component.ts
+++ src/app/personagens/personagens.component.ts
@@ -1,4 +1,29 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
+
+interface Personagem{
+  id:string;
+  name:string;
+  alternative_names:[];
+  species:string;
+  gender:string;
+  house:string;
+  dateOalternate_actorsfBirth:string;
+  yearOfBirth:number;
+  wizard:boolean;
+  ancestry:string;
+  eyeColour:string;
+  hairColour:string;
+  wand:[];
+  patronus:string;
+  hogwartsStudent:boolean;
+  hogwartsStaff:boolean;
+  actor:string;
+  alternate_actors:[];
+  alive:boolean;
+  image:string;
+}
+
 
 @Component({
   selector: 'app-personagens',
@@ -6,5 +31,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./personagens.component.css']
 })
 export class PersonagensComponent {
+
+  listaPersonagens$: Personagem[]=[];
+
+  async getPersonagens(){
+
+    axios.get<Personagem[]>('https://hp-api.onrender.com/api/characters')
+    .then((response)=>{
+      this.listaPersonagens$ = response.data;
+    }).catch((error)=>{
+      console.log(error)
+    })
+  }
+
+  constructor(){
+    this.getPersonagens()
+  }
 
 }
